e1f5dace085e2e2bbc1a2613f3b7f5dc1f82adab
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-03-28 16:43:00 +0530

    add function in dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,8 +3,15 @@ import React, { useState } from "react";
 import SideBar from "../common/SideBar";
 import DashBoardData from "./DashBoardData";
 import DashboardHeader from "../common/DashboardHeader";
+import { useSearchParams } from "next/navigation";
+import NotificationBar from "./NotificationBar";
+import AcademicPerformance from "./AcademicPerformance";
+import FeeStatus from "./FeeStatus";
 const Dashboard = () => {
   const [open, setOpen] = useState(false);
+  const searchParams = useSearchParams();
+  const tab = searchParams.get("tab");
+
   return (
     <div className="flex w-full relative pl-[325px] max-xl:pl-[300px] max-lg:pl-0">
       <SideBar open={open} />
@@ -15,21 +22,27 @@ const Dashboard = () => {
             onClick={() => setOpen(!open)}
           >
             <span
-              className={`flex w-full h-0.5 rounded-3xl bg-black transition-all duration-300 ${open && "translate-x-10"
-                }`}
+              className={`flex w-full h-0.5 rounded-3xl bg-black transition-all duration-300 ${
+                open && "translate-x-10"
+              }`}
             ></span>
             <span
-              className={`flex w-full h-0.5 relative rounded-3xl bg-black transition-all duration-300 after:flex after:w-full after:h-0.5  after:rounded-3xl after:bg-black after:transition-all after:duration-300 top-0 left-0 ${open && "rotate-45 after:rotate-90"
-                }`}
+              className={`flex w-full h-0.5 relative rounded-3xl bg-black transition-all duration-300 after:flex after:w-full after:h-0.5  after:rounded-3xl after:bg-black after:transition-all after:duration-300 top-0 left-0 ${
+                open && "rotate-45 after:rotate-90"
+              }`}
             ></span>
             <span
-              className={`flex w-full h-0.5 rounded-3xl bg-black transition-all duration-300 ${open && "-translate-x-10"
-                }`}
+              className={`flex w-full h-0.5 rounded-3xl bg-black transition-all duration-300 ${
+                open && "-translate-x-10"
+              }`}
             ></span>
           </button>
           <DashboardHeader />
         </div>
-        <DashBoardData />
+        {tab === "dashboard" && <DashBoardData />}
+        {tab === "academic-performance" && <AcademicPerformance />}
+        {tab === "fee-status" && <FeeStatus />}
+        {tab === "notification" && <NotificationBar />}
       </div>
     </div>
   );
---
+++ src/components/dashboard/FeeStatus.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeeStatus = () => {
+  return <div>FeeStatus</div>;
+};
+
+export default FeeStatus;
