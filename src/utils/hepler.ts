e4ed6fb1d2ae417e961f6fa33016369e7e5ce030
Ankush Dhull <apple@Apples-MacBook-Air.local>
2025-03-29 14:33:46 +0530

    make student data

--- src/utils/hepler.ts
+++ src/utils/hepler.ts
@@ -185,3 +185,21 @@ export const STUDENTS_LIST = [
     ]
   },
 ];
+export const STUDENTS_PERFORMANCE_LIST = [
+  {
+    title: "Total Marks",
+    icon: "marksIcon",
+  },
+  {
+    title: "Attendance",
+    icon: "attendanceIcon",
+  },
+  {
+    title: "Grade",
+    icon: "gradeIcon",
+  },
+  {
+    title: "Percentage ",
+    icon: "percentageIcon",
+  },
+];
