6c0997a82239cfb92ade7185d19e250b54640181
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-03-31 10:31:57 +0530

    remove some spaceing

--- src/components/dashboard/academic-performance/StudentReport.tsx
+++ src/components/dashboard/academic-performance/StudentReport.tsx
@@ -35,7 +35,7 @@ const StudentReport = () => {
   return (
     <div className="grid grid-cols-2 gap-[30px] max-[1870px]:grid-cols-1">
       <div className="flex border border-solid border-light-blue-two rounded-2xl flex-col">
-        <div className="flex w-full items-center py-6 px-5 justify-between">
+        <div className="flex w-full items-center py-[21px] px-5 justify-between">
           <p className="text-2xl max-sm:hidden max-xl:text-text-xl max-lg:text-lg">
             {term}
           </p>
@@ -76,7 +76,7 @@ const StudentReport = () => {
                   }`}
                   key={index}
                 >
-                  <td className="py-[23px] leading-160 !text-dark-black pl-6 font-semibold">
+                  <td className="py-[22.8px] leading-160 !text-dark-black pl-6 font-semibold">
                     {item.subject}
                   </td>
                   <td className="pl-6 text-gray">
